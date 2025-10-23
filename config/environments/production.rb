f5a9ab5bd1a5042ffea843be26d7b79ea181b6cf
GabrielViniciusss <biel23061@gmail.com>
2025-10-23 13:17:31 -0300

    fix: use TaggedLogging.new with STDOUT logger to resolve NoMethodError in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,10 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  # Use a STDOUT logger wrapped with TaggedLogging (Rails 7.x compatible API)
+  stdout_logger = ActiveSupport::Logger.new($stdout)
+  stdout_logger.formatter = ::Logger::Formatter.new
+  config.logger = ActiveSupport::TaggedLogging.new(stdout_logger)
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!).
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
